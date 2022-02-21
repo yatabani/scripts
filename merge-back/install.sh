c145d8fcf3cb13908481b0ae39fd927c35d51d6d
Yasir Atabani <yasir@utelsd.com>
2022-02-21 19:56:44 +0300

    Remove install wget

--- merge-back/install.sh
+++ merge-back/install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install wget
-apt install wget
-
 # Install script
 wget -O /usr/local/sbin/mb https://raw.githubusercontent.com/yatabani/scripts/master/merge-back/mb
 chmod +x /usr/local/sbin/mb
